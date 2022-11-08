b80f4b8d0827d26ce04d5c777a14ccdda13d6dd5
sangeon22 <tkddjsdl33@hanmail.net>
2022-11-08 15:07:48 +0900

    Test: "HelloController 단위테스트"
    HelloController 테스트코드 작성

---
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package org.example.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = {HelloController.class})
+public class HelloControllerTest {
+    @Autowired
+    private MockMVC mvc;
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
