2a73f1b93c93da63b227906058000e4ae9a72f9a
sangeon22 <tkddjsdl33@hanmail.net>
2022-11-11 20:27:27 +0900

    Test: "게시판 CRUD API 기능(3) - 게시물 수정/조회 기능"
    게시물 수정 Post 테스트 코드 작성

--- src/test/java/org/example/springboot/web/PostsApiControllerTest.java
+++ src/test/java/org/example/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package org.example.springboot.web;
 import org.example.springboot.web.domain.posts.Posts;
 import org.example.springboot.web.domain.posts.PostsRepository;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -43,7 +46,7 @@ class PostsApiControllerTest {
                 .author("author")
                 .build();
 
-        String url = "http://localhost:"+port+"/api/v1/posts";
+        String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
@@ -55,7 +58,40 @@ class PostsApiControllerTest {
         List<Posts> posts = postsRepository.findAll();
         assertThat(posts.get(0).getTitle()).isEqualTo(title);
         assertThat(posts.get(0).getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void Posts_수정() {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "update title";
+        String expectedContent = "update content";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> posts = postsRepository.findAll();
+        assertThat(posts.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(posts.get(0).getContent()).isEqualTo(expectedContent);
     }
 
 }
